d23de0fb08d18324358fc95441593a2c53a54140
Tobias Toland <github@thinktt.e4ward.com>
2023-06-11 00:31:17 -0500

    wip adding gin to project

--- cmd/kingapi/main.go
+++ cmd/kingapi/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"github.com/thinktt/yowking/pkg/engine"
 	"github.com/thinktt/yowking/pkg/models"
 )
@@ -14,6 +16,30 @@ type Settings = models.Settings
 var testJson = models.TestJson
 
 func main() {
+
+	r := gin.Default()
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "API is healthy",
+		})
+	})
+
+	r.POST("/move", func(c *gin.Context) {
+		var settings Settings
+		if err := c.ShouldBindJSON(&settings); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		moveData, err := engine.GetMove(settings)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, moveData)
+	})
+
+	r.Run()
 
 	// marshall the settings json data
 	var settings Settings
@@ -30,6 +56,4 @@ func main() {
 		return
 	}
 	fmt.Println(moveData)
-	// wait 10 seconds before exiting
-	// time.Sleep(10 * time.Second)
 }
