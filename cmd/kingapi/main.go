461284d692e4f98de7125607cd9ccb1f2cdbb632
Tobias Toland <github@thinktt.e4ward.com>
2023-06-12 12:11:34 -0500

    basic start on creating a token

--- cmd/kingapi/main.go
+++ cmd/kingapi/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/thinktt/yowking/pkg/engine"
 	"github.com/thinktt/yowking/pkg/models"
 	"github.com/thinktt/yowking/pkg/personalities"
@@ -20,6 +22,32 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "API is healthy",
 		})
+	})
+
+	// get a jwt token
+	r.POST("/token", func(c *gin.Context) {
+		var (
+			key []byte
+			t   *jwt.Token
+			s   string
+		)
+
+		key = []byte("fake-key")
+		t = jwt.NewWithClaims(jwt.SigningMethodHS256,
+			jwt.MapClaims{
+				"iss":   "yeoldwizard.com",
+				"sub":   "bobuser",
+				"exp":   time.Now().Add(time.Hour * 24).Unix(),
+				"roles": []string{"amdin", "mover"},
+			})
+
+		s, err := t.SignedString(key)
+		if err != nil {
+			fmt.Println("There was an error signing the token: ", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"token": s})
 	})
 
 	r.POST("/move-req", func(c *gin.Context) {
