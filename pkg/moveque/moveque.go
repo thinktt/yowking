54056cb80caf0ac232acbeae837c822d80004549
Tobias Toland <github@thinktt.e4ward.com>
2023-09-20 07:37:52 -0500

    initializing nats connection is working

--- pkg/moveque/moveque.go
+++ pkg/moveque/moveque.go
@@ -1,21 +1,75 @@
 package moveque
 
 import (
+	"os"
+
+	"github.com/nats-io/nats.go"
+	"github.com/sirupsen/logrus"
 	"github.com/thinktt/yowking/pkg/models"
 )
 
-func GetMove(moveReq models.MoveReq) (models.MoveData, error) {
-	// result := make(chan MoveResult)
-	// wating := atomic.AddInt64(&waitingRequests, 1)
-	// total := atomic.AddInt64(&totalRequests, 1)
-	// fmt.Println("total request: ", total, "waiting request: ", wating)
-	// moveRequests <- MoveRequest{Settings: settings, Result: result}
-	// res := <-result
+var log = logrus.New()
 
+func init() {
+	token := os.Getenv("NATS_TOKEN")
+	if token == "" {
+		log.Fatal("NATS_TOKEN environment variable is not set")
+	}
+
+	natsUrl := os.Getenv("NATS_URL")
+	if natsUrl == "" {
+		log.Println("NATS_URL not set, using:", nats.DefaultURL)
+		natsUrl = nats.DefaultURL
+	} else {
+		log.Println("NATS_URL set to:", natsUrl)
+	}
+
+	nc, err := nats.Connect(natsUrl, nats.Token(token))
+	if err != nil {
+		log.Fatalf("Error connecting to NATS: %v", err)
+	}
+
+	// Create a JetStream Context
+	js, err := nc.JetStream()
+	if err != nil {
+		log.Fatalf("Error creating JetStream context: %v", err)
+	}
+
+	// Create move-req-stream
+	_, err = js.AddStream(&nats.StreamConfig{
+		Name:     "move-req-stream",
+		Subjects: []string{"move-req"},
+	})
+	if err != nil {
+		log.Printf("Failed to create stream: %v", err)
+	} else {
+		log.Println("move-req-stream found or created")
+	}
+
+	// Create move-res-stream
+	_, err = js.AddStream(&nats.StreamConfig{
+		Name:     "move-res-stream",
+		Subjects: []string{"move-res.*"},
+	})
+	if err != nil {
+		log.Printf("Failed to create stream: %v", err)
+	} else {
+		log.Println("move-res-stream found or created")
+	}
+}
+
+func GetMove(moveReq models.MoveReq) (models.MoveData, error) {
 	moveRes := models.MoveData{}
 
 	return moveRes, nil
 }
+
+// result := make(chan MoveResult)
+// wating := atomic.AddInt64(&waitingRequests, 1)
+// total := atomic.AddInt64(&totalRequests, 1)
+// fmt.Println("total request: ", total, "waiting request: ", wating)
+// moveRequests <- MoveRequest{Settings: settings, Result: result}
+// res := <-result
 
 // func GetQueLength() int64 {
 // 	return atomic.LoadInt64(&waitingRequests)
