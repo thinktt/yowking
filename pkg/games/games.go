1a42d16331ea5b06a37e7b491e888ef6a4326228
Tobias Toland <github@thinktt.e4ward.com>
2024-07-09 00:18:36 -0500

    generate and add ids to new games

--- pkg/games/games.go
+++ pkg/games/games.go
@@ -1,9 +1,11 @@
 package games
 
 import (
+	"crypto/rand"
 	"errors"
 	"fmt"
 
+	"github.com/akamensky/base58"
 	"github.com/notnil/chess"
 )
 
@@ -17,4 +19,24 @@ func CheckMoves(moves []string) error {
 		}
 	}
 	return nil
+}
+
+func GetGameID() (string, error) {
+	// Create a byte array of length 6
+	b := make([]byte, 6)
+
+	// Fill the byte array with random bytes
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+
+	// Encode the byte array to a Base58 string
+	encoded := base58.Encode(b)
+
+	// If the encoded length is 9, chop off the first letter
+	if len(encoded) == 9 {
+		encoded = encoded[1:]
+	}
+	return encoded, nil
 }
--- pkg/models/games2.go
+++ pkg/models/games2.go
@@ -5,7 +5,7 @@ type Player struct {
 	Type string `json:"type" binding:"required,oneof=cmp lichess"`
 }
 type Game2 struct {
-	ID          string   `json:"id" bson:"id" binding:"required,alphanum,min=8,max=8"`
+	ID          string   `json:"id" bson:"id"`
 	LichessID   string   `json:"lichessId" bson:"lichessId"`
 	CreatedAt   int64    `json:"createdAt" bson:"createdAt" `
 	LastMoveAt  int64    `json:"lastMoveAt" bson:"lastMoveAt" `
@@ -22,7 +22,7 @@ type Game2Historical struct {
 	LichessID   string   `json:"lichessId" bson:"lichessId"`
 	CreatedAt   int64    `json:"createdAt" bson:"createdAt" binding:"required"`
 	LastMoveAt  int64    `json:"lastMoveAt" bson:"lastMoveAt" binding:"required"`
-	Status      string   `json:"status" bson:"status" binding:"required,oneof=created started mate resign stalemate draw"`
+	Status      string   `json:"status" bson:"status" binding:"required,oneof=created mate resign stalemate draw"`
 	Winner      string   `json:"winner,omitempty" bson:"winner" binding:"required,oneof=none white black"`
 	Moves       string   `json:"moves" bson:"moves,omitempty" binding:"required"`
 	MoveList    []string `json:"moveList,omitempty" bson:"moveList"`
